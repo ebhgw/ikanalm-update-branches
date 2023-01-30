69de14eda97aa6f05e470c019c5d544aed79aade
Evelino Bomitali <evelino.bomitali@hogwart.it>
2023-01-30 09:36:26 +0100

    Test working with TestContainer and image from ../svn-docker Dockerfile

--- svnadapter/src/test/groovy/it/zebco/alm/svn/adapter/SvnTestAgainstTestContainerSvnServerSpec.groovy
+++ svnadapter/src/test/groovy/it/zebco/alm/svn/adapter/SvnTestAgainstTestContainerSvnServerSpec.groovy
@@ -1,39 +1,51 @@
 package it.zebco.alm.svn.adapter
 
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.images.builder.ImageFromDockerfile
 import org.testcontainers.spock.Testcontainers
-import org.testcontainers.utility.DockerImageName
 import spock.lang.Shared
 import spock.lang.Specification
 
 @Testcontainers
-class SvnTestAgainstRealSvnServerSpec extends Specification {
+class SvnTestAgainstTestContainerSvnServerSpec extends Specification {
+    private final static dockerPath = new File('../svn-docker').toPath()
+    private String host
+    private int port
 
     // init, expose port of the container
     @Shared
-    GenericContainer testSvnServer = new GenericContainer<>(DockerImageName.parse("ebomitali/local-svn-server"))
+    GenericContainer testSvnServer = new GenericContainer<>(
+            new ImageFromDockerfile()
+                    .withFileFromPath('.', dockerPath))
             .withExposedPorts(80, 3690)
 
     void setup() {
-        // Assume that we have Redis running locally?
-        //host = testSvnServer.host
-        //port = testSvnServer.firstMappedPort
-        testSvnServer.start()
-        String address = testSvnServer.host
-        Integer port = testSvnServer.firstMappedPort
+        //testSvnServer.start()
+        this.host = testSvnServer.host
+        this.port = testSvnServer.firstMappedPort
     }
 
     def "check svn ls on an existing directory" (String path, boolean res) {
         given:
-        String host = testSvnServer.host
-        int port = testSvnServer.firstMappedPort
         String repoRootUrl = "http://${host}:${port}/svn/test"
-        SvnLsCommand ls = new SvnLsCommandImpl()
+        SvnLsCommand ls = new SvnLsCommandImpl("http://$host:$port/svn/test", "test", "TestP4ss")
         expect:
         ls.exists(path) == res
         where:
         path | res
         "branches/correttiva"|true
         "branches/inexistent"|false
+    }
+
+    def "Check svn ls raising errors" () {
+        given:
+        String repoRootUrl = "http://${host}:${port}/svn/test"
+        SvnLsCommand ls = new SvnLsCommandImpl(repoRootUrl, 'unknown', 'unknown')
+        when:
+        ls.exists('branches/correttiva')
+        then:
+        final SvnAdapterException rte = thrown(SvnAdapterException)
+        // errorcode for wrong password
+        rte.message =~ /E170013/
     }
 }
