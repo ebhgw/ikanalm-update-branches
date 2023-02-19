419f81c23c3eeaba944ea40e11bccb9ec0dab1c8
Evelino Bomitali <evelino.bomitali@hogwart.it>
2023-02-19 22:25:03 +0100

    Modified docker containers

--- docker-svn/create-run-svn-on-docker.cmd
+++ docker-svn/create-run-svn-on-docker.cmd
@@ -1,3 +1,5 @@
+rem cd to correct directory
+cd D:\Workspace\rma\ikan\ikan-phase-lib-2\docker-svn
 rem Create the image
 docker build . -t ebomitali/local-svn-server
 rem run the image -p external/host:internal/container)
